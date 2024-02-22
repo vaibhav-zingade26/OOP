36733e85523ea518400c1d524ba4a4ae15635797
vaibhav zingade <vaibhav.zingade26@gmail.com>
2024-02-23 00:16:19 +0530

    alternate

---
+++ src/main/java/StringDemo/alternate.java
@@ -0,0 +1,20 @@
+package StringDemo;
+
+public class alternate {
+    public static void main(String[] args) {
+        String str ="abXdRFGY";
+        char [] c=str.toCharArray();
+
+
+        for(int i=0;i<str.length();i++){
+            if(i%2==0 && Character.isUpperCase(c[i])){
+                c[i]= '*' ;
+            }
+        }
+       /* for(char ab:c){
+            System.out.print(ab+"");
+        }*/
+        System.out.println(new String(c));
+    }
+
+}
--- src/main/java/StringDemo/anagrams.java
+++ src/main/java/StringDemo/anagrams.java
@@ -1,34 +1,51 @@
 package StringDemo;
 
+import javax.sql.rowset.serial.SQLOutputImpl;
 import java.util.Arrays;
 
 public class anagrams {
-    public static void main(String[] args) {
-        String str1 = "The Morse Code";
-        String str2 = "Here come dots";
+   public static void main(String[] args) {
 
-        str1 =str1.toLowerCase();
-        str2=str2.toLowerCase();
+       String s1= "Hello";
+       String s2 = "World";
 
-        if(str1.length()!= str2.length()){
-            System.out.println("The both words are not anagrams");
-        }else {
+       System.out.println( s1+s2);
 
-            char[] string1 = str1.toCharArray();
-            char[] string2 = str2.toCharArray();
 
-            Arrays.sort(string1);
-            Arrays.sort(string2);
-
-            if(Arrays.equals(string1, string2)){
-                System.out.println("The both words are anagrams");
-            }else {
-                System.out.println("The both words are not anagrams");
-            }
+       StringBuffer s4 = new StringBuffer("Vaibhav");
+       StringBuffer s5= new StringBuffer("zingade");
+       System.out.println(s4.append(s5));
+       System.out.println(s4);
 
 
 
 
-        }
-    }
+
+//        String str1 = "The Morse Code";
+//        String str2 = "Here come dots";
+//
+//        str1 =str1.toLowerCase();
+//        str2=str2.toLowerCase();
+//
+//        if(str1.length()!= str2.length()){
+//            System.out.println("The both words are not anagrams");
+//        }else {
+//
+//            char[] string1 = str1.toCharArray();
+//            char[] string2 = str2.toCharArray();
+//
+//            Arrays.sort(string1);
+//            Arrays.sort(string2);
+//
+//            if(Arrays.equals(string1, string2)){
+//                System.out.println("The both words are anagrams");
+//            }else {
+//                System.out.println("The both words are not anagrams");
+//            }
+//
+//
+//
+//
+//        }
+   }
 }
